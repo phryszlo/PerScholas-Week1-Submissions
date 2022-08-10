5a9cf98ae5c7afe44332866e84b313f9ff3b7a10
phryszlo <phryszlo@gmail.com>
2022-08-10 07:47:34 -0400

    Tuesday homework submission

---
+++ Labs/js-exercises/maths.js
@@ -0,0 +1,30 @@
+//first exercise
+let a = 4;
+let b = 6;
+let c = a + b;
+
+//second exercise
+let thisVar = 65;
+let thatVar = 14;
+let someVar = 88.2;
+let randomAddition = someVar + b;
+console.log(randomAddition);
+
+//third exercise - the Average
+let v0 = 6;
+let v1 = 2.5;
+let v2 = 3.4;
+let v3 = 0.55;
+let v4 = 9.4;
+let v5 = 12;
+let v6 = 13;
+let v7 = 0;
+let v8 = 84;
+let v9 = 12.1;
+
+let avgFirstFive = (v0 + v1 + v2 + v3 + v4) / 5;
+let avgLastFive = (v5 + v6 + v7 + v8 + v9) /5;
+let avgOfAvgs = (avgFirstFive + avgLastFive) / 2;
+console.log("The average of the averages is ", avgOfAvgs);
+
+
--- Labs/js-practice/syntax.js
+++
@@ -1,2 +0,0 @@
-let myName = "Jordan"
-console.log(myName)
